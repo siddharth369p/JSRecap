e5b3ec0ecb369709c9c504d7b6ca037644453cd3
Siddharth Pathak <149286254+siddharth369p@users.noreply.github.com>
2026-02-02 11:05:45 +0000

    conversion and comparison

---
+++ comparison.js
@@ -0,0 +1,12 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// output - false,false,true
+// compariosn convert null to a number,treating it as 0
+// === it is strict check ,it check with data type
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+//undefined always give a false value
---
+++ conversion.js
@@ -0,0 +1,19 @@
+let score="44"
+let valueinNumber=Number(score)
+// console.log(typeof (score))
+
+// "33"-number
+// "33abc"-nan(not a number)
+let sc=true;
+// console.log(sc)
+let je=Number(sc)
+// console.log(je)
+// console.log(typeof (je))
+//************----------------------- */
+// boolean => number (1)
+// in case of number to boolean
+// then 1=> true& 0=> false
+
+let john=99;
+console.log(++john)
+console.log(john++)
