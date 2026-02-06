646ef918c610daed9b3643afd7ca95275d98bf20
siddharth369p <sidpathak2002@gmail.com>
2026-02-06 20:26:27 +0530

    array commit

---
+++ Array.js
@@ -0,0 +1,10 @@
+const j=["jai","ncdn"]
+const r=["cjdbn","ncnck"]
+
+let ram=j.concat(r)
+console.log(ram)
+console.log(Array.from({name:"jai"}))
+
+console.log(Array.of(100,200,300))
+console.log(Array.isArray("ram"))
+console.log(Array.from("ram"))
