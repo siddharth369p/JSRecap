fafe86ddfa92db2044da4c2bc309c3563110637b
siddharth369p <sidpathak2002@gmail.com>
2026-02-06 22:28:15 +0530

    basics of object

---
+++ Object2.js
@@ -0,0 +1,11 @@
+// //singleton
+// const tinder=new object();
+// // or
+
+// const tinder1={}
+// console.log(tinder)
+// console.log(tinder1)
+const tinderUser={}
+     tinderUser.name="ram"
+     tinderUser.id="jAI@CBDISA"
+     console.log(tinderUser)
