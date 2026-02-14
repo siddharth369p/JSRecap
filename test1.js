7f57da56b0069b39910f0b58a43637baf5245390
siddharth369p <sidpathak2002@gmail.com>
2026-02-14 23:18:46 +0530

    uses map and resuce

---
+++ test1.js
@@ -0,0 +1,30 @@
+let numbers=[2,4,6,8,9];
+
+let result=numbers.map(num=> num*3);
+ console.log(result);
+
+let jai=["Ram","Shyam","vellam"]
+
+let row=jai.map((je)=>"hellow"+je)
+   console.log(row)
+
+
+
+
+// const handleSubmit = async () => {
+//   const res = await fetch("http://localhost:5000/api/contact", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//       name: "Siddharth",
+//       email: "sid@gmail.com",
+//       message: "Hello"
+//     })
+//   });
+
+//   const data = await res.json();
+
+//   alert("Message sent successfully!");
+// };
