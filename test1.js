f4298f50b9a3651d75e82eec1e9e7ce1e1eb81bb
siddharth369p <sidpathak2002@gmail.com>
2026-02-14 23:30:02 +0530

    test the filter function

--- test1.js
+++ test1.js
@@ -1,12 +1,20 @@
 let numbers=[2,4,6,8,9];
 
 let result=numbers.map(num=> num*3);
- console.log(result);
+//  console.log(result);
+
+//  let jen=numbers.filter((nums)=>nums/2==0)
+//    console.log(jen);
+   let jen = numbers.filter((nums) => nums % 2 == 0)
+console.log(jen);
+
+
+
 
 let jai=["Ram","Shyam","vellam"]
 
 let row=jai.map((je)=>"hellow"+je)
-   console.log(row)
+  //  console.log(row)
 
 
 
